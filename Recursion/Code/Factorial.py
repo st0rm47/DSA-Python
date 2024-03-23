6f102399a4b60a4cbe0fae8ed968f6756cf4dfa8
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-03-23 11:58:16 +0545

    Factorial of Number

---
+++ Recursion/Code/Factorial.py
@@ -0,0 +1,10 @@
+# Factorial of a number using recursion
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+n = int(input("Enter a number: "))
+print("Factorial of",n ,"is:",factorial(n))
