fad9ea3959752a1e126fc6400dc8bd34b6697011
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-05-01 17:18:53 +0545

    List Slices

---
+++ DSA/Code/List Slices/list_slicing.py
@@ -0,0 +1,12 @@
+animals = ['dog', 'cat', 'guinea pig', 'rat']
+# extract elements using list slicing
+new_animals = animals[1:3]
+print(new_animals)
+# Output: ['cat', 'guinea pig']
+
+
+animals = ['dog', 'cat', 'guinea pig', 'rat']
+# extract elements using list slicing
+new_animals = animals[2:]
+print(new_animals)
+# Output: ['guinea pig', 'rat']
