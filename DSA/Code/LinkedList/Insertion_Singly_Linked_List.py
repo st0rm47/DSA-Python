e1f26532850456b08d0267e27484c6606d5a4ac5
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-06-21 17:57:54 +0545

    Insertion of Singly Linked List

---
+++ DSA/Code/LinkedList/Insertion_Singly_Linked_List.py
@@ -0,0 +1,42 @@
+# create the Node class
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+#Insertion at the End of the Linked List    
+def insert_at_end(self,data):
+    # create a new node
+    new_node = Node(data)
+    # set current to head
+    current = self.head
+    # traverse to the last node
+    while current.next is not None:
+        # move to the next node
+        current = current.next
+    # link the new node to the last node
+    current.next = new_node
+
+#Insertion at the Beginning of the Linked List
+def insert_at_beginning(self,data):
+    # create a new node
+    new_node = Node(data)
+    # link the new node to the head
+    new_node.next = self.head
+    # set the head to the new node
+    self.head = new_node
+
+#Insertion at the Given Position of the Linked List
+def insert_at_position(self,data,position):
+    # create a new node
+    new_node = Node(data)
+    # set current to head
+    current = self.head
+    for i in range(position-2):
+        # move to the next node
+        current = current.next
+    # link the new node to the next node
+    new_node.next = current.next
+    # link the new node to the current node
+    current.next = new_node
+    
